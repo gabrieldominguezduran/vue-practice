ae9d47fd44de5521e86cdc1dbfc26ebc82732f12
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2023-10-09 14:15:08 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Form />
+</template>
+
+<script>
+
+import {defineComponent} from "vue";
+import Form from "@/components/Form.vue";
+
+export default defineComponent({
+  components: {Form}
+})
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,12 @@
+<template>
+   This is my new Form
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style scoped></style>
